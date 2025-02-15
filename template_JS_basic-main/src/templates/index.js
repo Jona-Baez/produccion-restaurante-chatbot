e1a1294bfdcb421ad642cbff059cde0d6e16010c
Jona Baez <contactoconingenium@gmail.com>
2025-02-15 08:52:00 -0600

    creacion de index y carpetas

--- template_JS_basic-main/src/templates/index.js
+++
@@ -1,6 +0,0 @@
-import { createFlow } from '@builderbot/bot';
-import { welcomeFlow } from './welcomeFlow.js'
-
-export default createFlow([
-    welcomeFlow,
-]);
--- template_JS_basic-main/src/templates/welcomeFlow.js
+++
@@ -1,8 +0,0 @@
-import { addKeyword, EVENTS } from '@builderbot/bot';
-
-const welcomeFlow = addKeyword(EVENTS.WELCOME)
-    .addAction(async (ctx, ctxFn) => {
-        return ctxFn.endFlow("Bienvenido a este chatbot!")
-    })
-
-export { welcomeFlow };
