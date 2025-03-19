9e725219d9654c2a4998e446471458fa76d4c9c3
Jona Baez <168143760+Jona-Baez@users.noreply.github.com>
2025-03-18 20:24:49 -0600

    Delete base-ts-meta-memory directory

--- base-ts-meta-memory/src/templates/list_templates/sendimageFlow.ts
+++
@@ -1,8 +0,0 @@
-import { addKeyword } from "@builderbot/bot";
-
-const sendImageFlow = addKeyword("GS0310972")
-    .addAnswer("Te adjunto una imagen", {
-        media: "./assets/logo.png"
-    })
-
-export { sendImageFlow};
