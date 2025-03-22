70bb4c5f5fb7f994e9b4e334a025eb66485a77da
Jona Baez <contactoconingenium@gmail.com>
2025-03-21 20:12:21 -0600

    edicion en mainFlow

--- base-ts-meta-memory/src/templates/list_templates/unoFlow.ts
+++ base-ts-meta-memory/src/templates/list_templates/unoFlow.ts
@@ -1,8 +1,8 @@
 import { addKeyword, EVENTS } from "@builderbot/bot";
 
 const unoFlow = addKeyword("smartia119401")//Es la clave que invoca la opcion de la lista
-    .addAnswer("Te enviamos la Historia de la Virgen de Guadalupe en México *GRATIS*", {
-        media: "./assets/historiadelavirgen.pdf"
+    .addAnswer("Te enviamos nuestra carta", {
+        media: "./assets/carta.pdf"
     })
 
 export { unoFlow };
