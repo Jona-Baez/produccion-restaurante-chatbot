49dc2315ff3629b17a847cf22d824a910f0eeb1a
Jona Baez <contactoconingenium@gmail.com>
2025-03-21 20:09:18 -0600

    edicion en mainFlow

--- base-ts-meta-memory/src/templates/list_templates/dosFlow.ts
+++ base-ts-meta-memory/src/templates/list_templates/dosFlow.ts
@@ -1,5 +1,6 @@
 import { addKeyword } from "@builderbot/bot";
 
 const dosFlow= addKeyword("smartia119402")
-    .addAnswer("Da clic en en enlace de la sucursale que te interesa acudir:")
+    .addAnswer("Da clic en el enlace de la sucursal que te interesa acudir:")
+    .addAnswer("📍 Sucursal Zocalo: https://g.co/kgs/q1L6h4y")
 export { dosFlow };
