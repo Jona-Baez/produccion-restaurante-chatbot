32cdb6f4540627c2acc97a639561583d80e8cf54
Jona Baez <contactoconingenium@gmail.com>
2025-03-21 20:21:36 -0600

    edicion en mainFlow

--- base-ts-meta-memory/src/templates/list_templates/tresFlow.ts
+++ base-ts-meta-memory/src/templates/list_templates/tresFlow.ts
@@ -1,5 +1,8 @@
 import { addKeyword } from "@builderbot/bot";
 
 const tresFlow= addKeyword("smartia119403")
-    .addAnswer("💙🙏Lleva contigo un devocionario especial con 30 plegarias y oraciones dedicadas a la Virgen de Guadalupe.\n\n📖✨📿 Cada día, una oración diferente para pedir su protección, agradecer sus bendiciones y fortalecer tu devoción. \n\n💳 Realiza un deposito o transferencia de *$45 pesos* a nombre Jonathan Baez a la tarjeta 4027 6658 8507 5698 de Banco Azteca\n\n📝 Coloca en el concepto tu nombre para ubicar el pago\n\n📥 Envía tu comprobante de pago al numero 5528659461 y recibe el Ebook directo a tu WhastApp")
+    .addAnswer("Marca a los numeros correspondientes para hacer tu reserva:")
+    .addAnswer("📍 Sucursal Zocalo: \n\n Tel: 55 42 41 40 49")
+    .addAnswer("📍 Sucursal Veracruz: \n\n Tel:  228 241 40 49")
+    .addAnswer("📍 Sucursal Oaxaca: \n\n Tel:  222 241 40 49")
 export { tresFlow };
